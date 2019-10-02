0176177f9094550a62961d75f6fdd560b3d3a78c
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-10-02 12:59:11 +0200

    add Routing to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Switch,Route} from "react-router-dom";
+import Home from './Pages/Home'
+import Menu from "./Components/Menu/Menu";
 import DataTest from "./Pages/DataTest";
 
-function App() {
-  return (
-    <DataTest/>
-  );
+class App extends React.Component{
+
+  constructor(){
+    super()
+  }
+
+
+  render(){
+    return (
+        <div>
+
+        <BrowserRouter>
+            <Menu/>
+            <Switch>
+              <Route path="/" exact render={()=> <Home /> } />
+              <Route path="/DataTest" render={()=><DataTest/>}/>
+            </Switch>
+          </BrowserRouter>
+        </div>
+    );
+  }
+
 }
 
 export default App;
