d2d7f75b9ee99c07ebd3ceaaa5c2734e5fa1d845
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-10-04 18:52:20 +0200

    Custom Logo

--- client/src/Pages/Rooster.jsx
+++ client/src/Pages/Rooster.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import RoosterComponent from "../Components/Rooster/RoosterComponent";
-
 class Rooster extends React.Component{
     constructor(){
         super()
+        this.state={
+            agendaJSON:[]
+        }
+    }
+
+    componentDidMount=async ()=> {
+        //Hier wordt de data uit de server gehaald en in de state gezet
+        var res=await fetch(this.props.apiLink+"/api/getAgenda/2").catch(reason => {console.log(reason)})
+        var agendaJSON=await res.json()
+        this.setState({agendaJSON:agendaJSON})
+
     }
 
     render() {
         return (
             <div>
-                <RoosterComponent startDate={new Date(2019,8,30,0,0,0)} beginTijd={10} eindTijd={24} height={500}/>
+                <RoosterComponent markerInterval={new Date(0,0,0,0,30)} agendaJSON={this.state.agendaJSON} startDate={new Date(2019,8,30,0,0,0)} beginTijd={new Date(0,0,0,7)} eindTijd={new Date(0,0,0,20)} height={600}/>
             </div>
         );
     }
