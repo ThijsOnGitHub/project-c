ea8856d132338020960d7484dbec3a2732ea5026
Delano-Koster <0928340@hr.nl>
2019-10-06 13:58:22 +0200

    Added loginscreen and style

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -8,9 +8,31 @@ class Home extends React.Component{
 
     render() {
         return(
+<<<<<<< Updated upstream
         <div>
 
             <p>Dit is de Homepage</p>
+=======
+        <div id="intro">
+            <form>
+            <table>
+                <tbody>
+                <tr>
+                    <td><input type="text" id="uname" name="uname" value="Gebruikersnaam"/></td>
+                </tr>
+                <tr>
+                    <td><input type="text" id="pass" name="pass" value="Wachtwoord"/></td>
+                </tr>
+                <tr>
+                    <td><a href="#">Wachtwoord vergeten?</a></td>
+                </tr>
+                <tr>
+                    <button>Inloggen</button>
+                </tr>
+                </tbody>
+            </table>
+            </form>
+>>>>>>> Stashed changes
         </div>
         )
     }
