4cc1c8b6736b6c5e9b4bdd6d1aa9b02b87b5eba9
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-10-07 12:10:08 +0200

    Make Loginpage React Friendly

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -4,6 +4,22 @@ import {Link} from "react-router-dom";
 class Home extends React.Component{
     constructor(){
         super()
+        this.state={
+            uname:"",
+            pass:""
+        }
+        this.handleInputChange=this.handleInputChange.bind(this)
+    }
+
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
     }
 
     render() {
@@ -13,10 +29,10 @@ class Home extends React.Component{
             <table>
                 <tbody>
                 <tr>
-                    <td><input type="text" id="uname" name="uname" value="Gebruikersnaam"/></td>
+                    <td><input type="text" id="uname" name="uname" placeholder="Gebruikersnaam" value={this.state.uname} onChange={this.handleInputChange} /></td>
                 </tr>
                 <tr>
-                    <td><input type="text" id="pass" name="pass" value="Wachtwoord"/></td>
+                    <td><input type="password" id="pass" name="pass" placeholder="Wachtwoord" value={this.state.pass} onChange={this.handleInputChange} /></td>
                 </tr>
                 <tr>
                     <td><a href="#">Wachtwoord vergeten?</a></td>
