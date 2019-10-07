8e369d58d58ff1c439489d8d67a6f3bbf0d156fe
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-10-07 12:27:23 +0200

    Revert "add options"
    This reverts commit e6a60a5300ca2c5383e46cffcd46298ea3eeb52a.

--- client/src/Components/Rooster/WerknemerItem.jsx
+++ client/src/Components/Rooster/WerknemerItem.jsx
@@ -10,7 +10,7 @@ class WerknemerItem extends React.Component{
         return(
             <div className="column isideItem">
                 <div className="row">
-                    <p className="onAccent noTopMargin">{new Date(this.props.itemData.beginTijd).toLocaleTimeString("nl-NL",{hour:"2-digit",minute:"2-digit"})}-{new Date(this.props.itemData.eindTijd).toLocaleTimeString("nl-NL",{hour:"2-digit",minute:"2-digit"})}</p>
+                    <p className="onAccent noTopMargin">{new Date(this.props.itemData.beginTijd).toLocaleTimeString("nl-NL",{timeZone:"UTC",hour:"2-digit",minute:"2-digit"})}-{new Date(this.props.itemData.eindTijd).toLocaleTimeString("nl-NL",{timeZone:"UTC",hour:"2-digit",minute:"2-digit"})}</p>
                     <MoreOptions width={35} height={35} className="onAccent right"/>
                 </div>
             </div>
