aadc744c02a0263883e55d09d99b757794cafab5
hichamad <43321370+hichamad@users.noreply.github.com>
2019-10-08 23:33:27 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,58 @@
 import React, { Component }from 'react';
-import './App.css';
-import logo from './logo.svg'
+import './App.scss';
 import {BrowserRouter,Switch,Route} from "react-router-dom";
 import Home from './Pages/Home'
 import Menu from "./Components/Menu/Menu";
 import DataTest from "./Pages/DataTest";
-
+import Loginpage from "./Pages/Loginpage";
+import{login, Register} from "./Pages/index.txs";
+import Registerpage from "./Pages/Registerpage";
 
 
 class App extends React.Component {
-
-    constructor() {
-        super()
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLogginActive: true
+        };
     }
 
+    render() {
+        const { isLogginActive } = this.state;
+        const current = isLogginActive ? "Register" : "Login";
+        return (
+            <div className="App">
+                <div className="login">
+                    <div className="container" ref={ref => (this.container = ref)}>
+                        {isLogginActive && (
+                            <login containerRef={ref => (this.current = ref)} />
+                        )}
+                        {!isLogginActive && (
+                            <Register containerRef={ref => (this.current = ref)} />
+                            )}
+                        )}
+                        <div>
+
+                            <BrowserRouter>
+                                <Menu/>
+                                <Switch>
+                                    <Route path="/" exact render={() => <Home/>}/>
+                                    <Route path="/DataTest" render={() => <DataTest/>}/>
+                                    <Route path="/login" render={() => <Loginpage/>}/>
+                                    <Route path="/Register" render={() => <Registerpage/>}/>
+                                </Switch>
+                            </BrowserRouter>
 
 
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-render()
-{
-    return (
-        <div>
 
-            <BrowserRouter>
-                <Menu/>
-                <Switch>
-                    <Route path="/" exact render={() => <Home/>}/>
-                    <Route path="/DataTest" render={() => <DataTest/>}/>
-                </Switch>
-            </BrowserRouter>
-        </div>
-    );
-  }
+        );
+    }
 }
+
 
 export default App
