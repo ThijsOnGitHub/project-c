2d2421c7eeca4011a20c0cc53fc56b351e3b22fd
hichamad <43321370+hichamad@users.noreply.github.com>
2019-10-11 21:05:28 +0200

    Update Home.jsx

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,51 +1,38 @@
 import React from 'react'
 import {Link} from "react-router-dom";
 
+
 class Home extends React.Component{
-    constructor(){
-        super()
-        this.state={
-            uname:"",
-            pass:""
+    constructor(props){
+        super(props);
+
         }
-        this.handleInputChange=this.handleInputChange.bind(this)
+
+
+    render(){
+        return (
+            <div className="base-container" ref={this.props.containerRef}>
+                <div className="header">login</div>
+                <div className="content">
+                    <br/>
+                    <div className="form">
+                        <div className="form-group">
+                            <label htmlFor="username">Username</label>
+                            <input type="text" name="username" placeholder="username" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" name="password" placeholder="password" />
+                        </div>
+                    </div>
+                </div>
+                <div className="footer">
+                    <button type="button" className="btn">
+                        Login
+                    </button>
+                </div>
+            </div>
+        );
     }
-
-
-    handleInputChange(event) {
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
-
-        this.setState({
-            [name]: value
-        });
-    }
-
-    render() {
-        return(
-        <div id="intro">
-            <form>
-            <table>
-                <tbody>
-                <tr>
-                    <td><input type="text" id="uname" name="uname" placeholder="Gebruikersnaam" value={this.state.uname} onChange={this.handleInputChange} /></td>
-                </tr>
-                <tr>
-                    <td><input type="password" id="pass" name="pass" placeholder="Wachtwoord" value={this.state.pass} onChange={this.handleInputChange} /></td>
-                </tr>
-                <tr>
-                    <td><a href="#">Wachtwoord vergeten?</a></td>
-                </tr>
-                <tr>
-                    <button>Inloggen</button>
-                </tr>
-                </tbody>
-            </table>
-            </form>
-        </div>
-        )
-    }
-
 }
 export default Home
