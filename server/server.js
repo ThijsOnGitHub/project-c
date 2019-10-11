e0d6a06f0e21024004716b9f63453f43d23fc633
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-10-11 12:39:58 +0200

    Add test functie

--- server/server.js
+++ server/server.js
@@ -49,7 +49,9 @@ app.post("/api/addbedrijf",(req,res)=>{
     })
 })
 
-
+app.get("/api/test",(req,res)=>{
+    res.status(200).send("Hello!")
+})
 
 // Zend een POST request dat de data uit de front-end in de database krijgt.
 app.post("/api/addgebruiker", (req, res) => {
