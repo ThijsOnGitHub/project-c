79dee8e5b0709e71a2d74a233b05c6008b6c3ffb
hichamad <43321370+hichamad@users.noreply.github.com>
2019-10-13 16:20:46 +0200

    Update Home.jsx

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,64 +1,51 @@
 import React from 'react'
 import {Link} from "react-router-dom";
-var API_LINK='http://localhost:5000/api';
-
-
 
 class Home extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-         email: '',
-         pass: ''
-        };
+    constructor(){
+        super()
+        this.state={
+            uname:"",
+            pass:""
+        }
+        this.handleInputChange=this.handleInputChange.bind(this)
+    }
 
-     this.lijst=["email", "pass"];
-     this.handleSubmit = this.handleSubmit.bind(this);
-     }
 
-    handleSubmit() {
-        var object = {};
-        this.lijst.forEach((value) => {
-            object[value] = this.state[value]
-        });
-        console.log("sending");
-        console.log(object);
-        fetch(API_LINK+"/addgebruiker",{method:"POST",
-            body:JSON.stringify(object),
-            headers:{
-                "content-type":"application/json"
-            }}).then((value)=>{
-            value.json().then(value1 => {console.log(value1.message)})
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
         });
     }
 
-        render(){
-            return (
-
-                <div className="base-container" ref={this.props.containerRef}>
-                    <div className="header">Login</div>
-                    <div className="content">
-                        <br/>
-                        <div className="form">
-                            <div className="form-group">
-                                <label htmlFor="username">Email</label>
-                                <input type="text" name="Username" placeholder="Email"/>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="password">Password</label>
-                                <input type="password" name="password" placeholder="password"/>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="footer">
-                        <button type="button" className="btn">
-                            Login
-                        </button>
-                    </div>
-                </div>
-            );
-        }
-
+    render() {
+        return(
+            <div id="intro">
+                <form>
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td><input type="text" id="uname" name="uname" placeholder="Gebruikersnaam" value={this.state.uname} onChange={this.handleInputChange} /></td>
+                        </tr>
+                        <tr>
+                            <td><input type="password" id="pass" name="pass" placeholder="Wachtwoord" value={this.state.pass} onChange={this.handleInputChange} /></td>
+                        </tr>
+                        <tr>
+                            <td><a href="#">Wachtwoord vergeten?</a></td>
+                        </tr>
+                        <tr>
+                            <button>Inloggen</button>
+                        </tr>
+                        </tbody>
+                    </table>
+                </form>
+            </div>
+        )
+    }
 
 }
 export default Home
