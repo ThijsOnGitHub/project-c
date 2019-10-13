00031171a7880dbb428a0471c8f1eb28d346031d
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-10-13 21:58:40 +0200

    Add options to roosterItems

--- client/src/Components/Rooster/RoosterItems/WerknemerItem.jsx
+++ client/src/Components/Rooster/RoosterItems/WerknemerItem.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {ReactComponent as MoreOptions} from "../../../icons/more_horiz-24px.svg";
+import OptionWithIcon from "../../OptionWithIcon";
+
 
 class WerknemerItem extends React.Component{
     constructor(){
@@ -10,8 +12,17 @@ class WerknemerItem extends React.Component{
         return(
             <div className="column isideItem">
                 <div className="row">
-                    <p className="onAccent noTopMargin">{new Date(this.props.itemData.beginTijd).toLocaleTimeString("nl-NL",{hour:"2-digit",minute:"2-digit"})}-{new Date(this.props.itemData.eindTijd).toLocaleTimeString("nl-NL",{hour:"2-digit",minute:"2-digit"})}</p>
-                    <MoreOptions width={35} height={35} className="onAccent right"/>
+                    <p className="onAccent noMargin">{new Date(this.props.itemData.beginTijd).toLocaleTimeString("nl-NL",{hour:"2-digit",minute:"2-digit"})}-{new Date(this.props.itemData.eindTijd).toLocaleTimeString("nl-NL",{hour:"2-digit",minute:"2-digit"})}</p>
+                    <details className="chooseMenu right" >
+                        <div>
+                            <OptionWithIcon icon="people-24px.svg" text="Vervanging Regelen"/>
+                            <OptionWithIcon icon="local_hospital-24px.svg" text="Ziek Melden"/>
+                            <OptionWithIcon icon="disable_person.svg" text="Vrij Vragen"/>
+                        </div>
+                        <summary>
+                                <MoreOptions width={35} height={35} className="onAccent right"/>
+                        </summary>
+                    </details>
                 </div>
             </div>
         )
