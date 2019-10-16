0807dbf9f9ad8e5d0ba8b8efe88d01e0e6aace2b
Tamme40K <berendvanloon12@gmail.com>
2019-10-16 12:41:01 +0200

    Inputvalidatie, weergave errors
    Validatie of er iets in een veld staat. Errors weergegeven door rode randen en door de submit-knop te blurren.

--- client/src/Pages/Registratie.jsx
+++ client/src/Pages/Registratie.jsx
@@ -13,26 +13,50 @@ class Registratie extends React.Component{
             phone: '',
             birth: '',
             img_link: '',
-            isWerkgever: false
+            isWerkgever: false,
+            // Sla op of inputvelden al zijn aangeraakt door de gebruiker.
+            touched: {
+                firstName: false,
+                lastName: false,
+                email: false,
+                pass: false,
+                phone: false,
+                birth: false,
+                img_link: false
+            }
         };
         // Lijst om uit te lezen voor het POST request.
-        this.lijst=["firstName","lastName","email","pass","phone","birth","img_link", "isWerkgever"];
+        this.lijst = ["firstName","lastName","email","pass","phone","birth","img_link", "isWerkgever"];
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    // Controlleer of de waarden in een veld wel verstuurd kunnen worden.
+    canBeSubmitted() {
+        const errors = this.validate(this.state.firstName, this.state.lastName, this.state.email, this.state.pass, this.state.phone, this.state.birth, this.state.img_link);
+        const isDisabled = Object.keys(errors).some(x => errors[x]);
+        return !isDisabled;
+    }
+
     // Converteer de waarden uit de state naar een JSON string om die in een POST request te plaatsen en te versturen.
-    handleSubmit() {
-        var object={};
-        this.lijst.forEach((value)=>{
-            var returnValue=this.state[value]
-            if(typeof returnValue==='boolean'){
-                returnValue=returnValue?1:0
-            }else if (value==="birth"){
-                returnValue=new Date(returnValue).toLocaleDateString('en-US',{year:'2-digit',month:"2-digit",day:"2-digit"},"UTC")
+    handleSubmit(event) {
+        // Laat de data niet verstuurd worden wanneer de input validatie niet succesvol is.
+        if (!this.canBeSubmitted()) {
+            event.preventDefault();
+            return;
+        }
+
+        let object = {};
+        this.lijst.forEach((value) => {
+            let returnValue = this.state[value];
+            if (typeof returnValue === 'boolean') {
+                returnValue = returnValue ? 1 : 0;
+            } else if (value === "birth") {
+                returnValue = new Date(returnValue).toLocaleDateString('en-US', {year:'2-digit', month:"2-digit", day:"2-digit"}, "UTC");
             }
-            object[value]=returnValue
+            object[value] = returnValue;
         });
+
         console.log("sending");
         console.log(object);
         fetch(this.props.apiLink+"/api/addgebruiker",{method:"POST",
@@ -53,8 +77,38 @@ class Registratie extends React.Component{
         this.setState({[name]: value});
     }
 
+    validate(firstName, lastName, email, pass, phone, birth, img_link) {
+        // Als een waarde hier true is betekent dat dat het veld niet valide is.
+        return {
+            firstName: firstName.length === 0,
+            lastName: lastName.length === 0,
+            email: email.length === 0,
+            pass: pass.length === 0,
+            phone: phone.length === 0,
+            birth: birth.length === 0,
+            img_link: img_link.length === 0
+        };
+    }
+
+    // Verander de waarde van touched voor een inputveld naar true.
+    handleBlur = (field) => (event) => {
+        this.setState({
+            touched: {...this.state.touched, [field]: true},
+        });
+    };
+
     // Verzamel de inputs van de gebruiker om die in de state op te slaan.
     render() {
+        const errors = this.validate(this.state.firstName, this.state.lastName, this.state.email, this.state.pass, this.state.phone, this.state.birth, this.state.img_link);
+        const isDisabled = Object.keys(errors).some(x => errors[x]);
+
+        // Valideer of een fout getoond zou moeten worden.
+        const shouldMarkError = (field) => {
+            const hasError = errors[field];
+            const shouldShow = this.state.touched[field];
+            return hasError ? shouldShow : false;
+        };
+
         return(
         <div id="reg">
             <form>
@@ -62,37 +116,51 @@ class Registratie extends React.Component{
                 <tbody>
                 <tr>
                     <label>Voornaam</label>
-                    <td><input type='text' name="firstName" value={this.state.firstName} placeholder="Voornaam" onChange={this.handleInputChange}/></td>
+                    <td><input className={shouldMarkError('firstName') ? "error" : ""}
+                               onBlur={this.handleBlur('firstName')}
+                               type='text' name="firstName" value={this.state.firstName} placeholder="Voornaam" onChange={this.handleInputChange}/></td>
                 </tr>
                 <tr>
                     <label>Achternaam</label>
-                    <td><input type='text' name="lastName" value={this.state.lastName} placeholder="Achternaam" onChange={this.handleInputChange}/></td>
+                    <td><input className={shouldMarkError('lastName') ? "error" : ""}
+                               onBlur={this.handleBlur('lastName')}
+                               type='text' name="lastName" value={this.state.lastName} placeholder="Achternaam" onChange={this.handleInputChange}/></td>
                 </tr>
                 <tr>
                     <label>Email</label>
-                    <td><input type='email' name="email" value={this.state.email} placeholder="Email" onChange={this.handleInputChange}/></td>
+                    <td><input className={shouldMarkError('email') ? "error" : ""}
+                               onBlur={this.handleBlur('email')}
+                               type='email' name="email" value={this.state.email} placeholder="Email" onChange={this.handleInputChange}/></td>
                 </tr>
                 <tr>
                     <label>Telefoonnummer</label>
-                    <td><input type='text' name="phone" value={this.state.phone} placeholder="Telefoonnummer" onChange={this.handleInputChange}/></td>
+                    <td><input className={shouldMarkError('phone') ? "error" : ""}
+                               onBlur={this.handleBlur('phone')}
+                               type='text' name="phone" value={this.state.phone} placeholder="Telefoonnummer" onChange={this.handleInputChange}/></td>
                 </tr>
                 <tr>
                     <label>Geboortedatum</label>
-                    <td><input type='date' name="birth" value={this.state.birth} placeholder="Geboortedatum" onChange={this.handleInputChange}/></td>
+                    <td><input className={shouldMarkError('birth') ? "error" : ""}
+                               onBlur={this.handleBlur('birth')}
+                               type='date' name="birth" value={this.state.birth} placeholder="Geboortedatum" onChange={this.handleInputChange}/></td>
                 </tr>
                 <tr>
                     <label>URL gebruikersafbeelding</label>
-                    <td><input type='text' name="img_link" value={this.state.img_link} placeholder="URL gebruikersafbeelding" onChange={this.handleInputChange}/></td>
+                    <td><input className={shouldMarkError('img_link') ? "error" : ""}
+                               onBlur={this.handleBlur('img_link')}
+                               type='text' name="img_link" value={this.state.img_link} placeholder="URL gebruikersafbeelding" onChange={this.handleInputChange}/></td>
                 </tr>
                 <tr>
                     <label>Wachtwoord</label>
-                    <td><input type='password' name="pass" value={this.state.pass} placeholder="Wachtwoord" onChange={this.handleInputChange}/></td>
+                    <td><input className={shouldMarkError('pass') ? "error" : ""}
+                               onBlur={this.handleBlur('pass')}
+                               type='password' name="pass" value={this.state.pass} placeholder="Wachtwoord" onChange={this.handleInputChange}/></td>
                 </tr>
                 <tr>
                     <label>Account voor werkgever</label>
                     <td><input type='checkbox' name="isWerkgever" value={this.state.isWerkgever} placeholder="false" onChange={this.handleInputChange}/></td>
                 </tr>
-                <button onClick={this.handleSubmit}>Registreer</button>
+                <button disabled={isDisabled} onClick={this.handleSubmit}>Registreer</button>
                 </tbody>
             </table>
             </form>
