3a43691e51842af65b49cd6f2d2daf2580d657f0
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-10-18 11:54:55 +0200

    fix error

--- server/server.js
+++ server/server.js
@@ -81,12 +81,14 @@ app.post("/api/addgebruiker",async (req, res) => {
 
 app.post("/api/Login", (req,res) =>{
     console.log(req.body);
-    connection.query("SELECT email,pass FROM gebruiker where email =?",[req.body.email],(err,values,fields)=>{
+    connection.query("SELECT email,pass FROM gebruiker where email = ?",[req.body.email],(err,values,fields)=>{
         if(err){
             res.send("Not valid")
         }
-
-        bcrypt.compare(req.body.pass,values.pass,(res)=>{
+        bcrypt.compare(req.body.pass,values[0].pass,(err,res)=>{
+            if(err){
+                console.error(err)
+            }
             console.log(res)
 
         })
