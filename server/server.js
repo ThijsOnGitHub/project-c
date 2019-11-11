85d37f953d89fe49b2633f085fd64ec34a3b6ade
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-11-11 13:15:21 +0100

    fix

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors= require('cors');
-serverLogin=require('./serverlogin');
+
 const cookieParser = require('cookie-parser')
 
 serverSecret=require('./serverSecret');
