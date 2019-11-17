0b7f97c934fb4be4cf9f679bf06a3bdce31bc267
hichamad <43321370+hichamad@users.noreply.github.com>
2019-11-17 14:11:04 +0100

    Update WerknemerItem.tsx

--- client/src/Components/Rooster/RoosterItems/WerknemerItem.tsx
+++ client/src/Components/Rooster/RoosterItems/WerknemerItem.tsx
@@ -9,6 +9,12 @@ interface IProps {
 
 class WerknemerItem extends React.Component<IProps>{
 
+
+
+        handleSubmit=async (event:React.MouseEvent<HTMLButtonElement,MouseEvent>)=> {
+            this.setState({loading: true})
+            event.preventDefault()
+        }
     render() {
         return(
             <div className="column isideItem">
@@ -16,10 +22,12 @@ class WerknemerItem extends React.Component<IProps>{
                     <p className="onAccent noMargin">{new Date(this.props.itemData.beginTijd).toLocaleTimeString("nl-NL",{hour:"2-digit",minute:"2-digit"})}-{new Date(this.props.itemData.eindTijd).toLocaleTimeString("nl-NL",{hour:"2-digit",minute:"2-digit"})}</p>
                     <details className="chooseMenu right" >
                         <div>
-                            <OptionWithIcon icon="people-24px.svg" text="Vervanging Regelen"/>
-                            <OptionWithIcon icon="local_hospital-24px.svg" text="Ziek Melden"/>
-                            <OptionWithIcon icon="disable_person.svg" text="Vrij Vragen"/>
+                            <button><OptionWithIcon icon="people-24px.svg"  text="Vervanging Regelen"/></button>
+
+                            <button><OptionWithIcon icon="local_hospital-24px.svg" text="Ziek Melden"/></button>
+                            <button><OptionWithIcon icon="disable_person.svg" text="Vrij Vragen"/></button>
                         </div>
+
                         <summary>
                                 <MoreOptions width={35} height={35} className="onAccent right"/>
                         </summary>
