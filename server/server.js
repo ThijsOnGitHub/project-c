335c1b8b52a5792f32f00ede990c76e7db837314
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-12-13 10:53:26 +0100

    Verwijder onnodige code

--- server/server.js
+++ server/server.js
@@ -20,36 +20,9 @@ app.use("/api",apiRoute);
 app.use("/auth",authRoute);
 
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname+'../client/build/index.html'));
 });
-/*
-app.post("/api/Login", (req,res) =>{
-    console.log(req.body);
-    connection.query("SELECT email,pass,id FROM gebruiker where email = ?",[req.body.email],(err,values,fields)=>{
-        console.log(values.length)
-        console.log(err)
 
-        if (err|| values.length === 0) {
-
-            res.status(401).send("Not valid")
-        }
-        else (bcrypt.compare(req.body.pass,values[0].pass,(err,result)=>{
-            if(err){
-                console.error(err)
-            }
-            console.log(result)
-            if(result){
-                var payLoad={id:values[0].id}
-                var sessionToken=jwt.sign(payLoad, serverSecret.serverSecret.sessionSecret,{expiresIn:"10s"});
-                var refeshToken=jwt.sign(payLoad,serverSecret.serverSecret.refreshSecret)
-                res.status(200).json({sessionToken:token})
-            }else{
-                res.status(401).send("Not valid")
-            }
-}))
-    })});
-
- */
 
 app.listen(5000,()=> {
     console.log("listening")
