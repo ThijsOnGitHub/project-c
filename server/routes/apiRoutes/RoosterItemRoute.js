09c89c6f3f4ed8fcd65cdf50aefcdee52d879b35
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2020-01-06 14:58:45 +0100

    Melding bij wijzigen ww

--- server/routes/apiRoutes/RoosterItemRoute.js
+++ server/routes/apiRoutes/RoosterItemRoute.js
@@ -25,9 +25,14 @@ app.post("/add",auth,(req,res)=>{
                         console.log("Add failed");
                         res.status(500).send(err)
                     }else {
-                        connection.query("INSERT INTO Notifications (userId, messageType, roosterId) VALUES (?,?,(select roosterId from gebruiker where id=?))", [req.user.userId, 3,req.user.userId], (error, results, fields) => {})
-                            res.status(200).send(values)
-                            console.log("Add succeed")
+                        connection.query("INSERT INTO Notifications (userId, messageType, roosterId) VALUES (?,3,(select roosterId from gebruiker where id=?))", [req.user.id,req.user.id], (error, results, fields) => {
+                            if(error){
+                                console.error(error)
+                            }
+                            console.log("notification added")
+                        })
+                        res.status(200).send(values)
+                        console.log("Add succeed")
                     }
                 })
             }else{
@@ -49,7 +54,6 @@ app.post("/change/:id",[auth,yourItem],(req, res) => {
         if(err){
             res.status(500).send(err)
         }else{
-
             connection.query("INSERT INTO Notifications (userId, messageType, roosterId,roosterItemId) VALUES (?,?,(select roosterId from gebruiker where id=?),?)", [req.user.id, 3,req.user.id,req.params.id], (error, results, fields) => {
                 if(error){
                     console.log(error)
@@ -72,7 +76,7 @@ app.delete("/remove/:id",[auth,yourItem],(req, res) => {
             console.log("Delete Failed")
         } else {
             console.log("Delete Done")
-            connection.query("INSERT INTO Notifications (userId, messageType, roosterId) VALUES (?,?,(select roosterId from gebruiker where id=?))", [req.user.userId, 3,req.user.userId], (error, results, fields) => {})
+            connection.query("INSERT INTO Notifications (userId, messageType, roosterId) VALUES (?,?,(select roosterId from gebruiker where id=?))", [req.user.id, 3,req.user.id], (error, results, fields) => {})
 
             res.status(200).send("Verwijderen Gelukt")
 
